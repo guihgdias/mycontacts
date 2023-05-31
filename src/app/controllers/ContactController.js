ad9c8d3eac7347928d60ba71964a2706b52ece8f
guihgdias <guilhermehgdiasbr@gmail.com>
2023-05-30 21:02:06 -0300

    First directories

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index(request, response) { // Listar todos os registros
+    response.send('Chama no send');
+  }
+
+  show() { // Obter um registro
+
+  }
+
+  store() { // Criar novo registro
+
+  }
+
+  update() { // Editar um registro
+
+  }
+
+  delete() { // Deletar um registro
+
+  }
+}
+
+module.exports = new ContactController();
